d18ed37541b27bdf880ee87eef93562cead8f603
CarlosArturoMT <carlosarturomt@gmail.com>
2021-05-27 02:10:27 -0500

    se agrega perfil de arturomt

--- js/toggle.js
+++ js/toggle.js
@@ -1,13 +1,21 @@
 let menuToggle = document.querySelector(".menu-toggle");
 let menuToggleIcon = document.querySelector(".menu-toggle i");
 let menu = document.getElementById("menu");
+let intro = document.getElementById('intro');
 
-menuToggle.addEventListener("click", e => {
+menuToggle.addEventListener("click", e =>   {
     menu.classList.toggle("show");
 
     if (menu.classList.contains("show")) {
         menuToggleIcon.setAttribute("class", "fa fa-times");
+        intro.classList.add("prueba--x");
+
     } else {
         menuToggleIcon.setAttribute("class", "fa fa-bars");
+        intro.style.backgroundColor = 'transparent'; 
+        // intro.classList.add("prueba--x1");
     }
 });
+
+
+
